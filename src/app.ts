f36d300fcabb4105fa1b467d458d959b70df890f
Unknown <joshcodedev@gmail.com>
2018-02-15 05:00:25 +0100

    Initialise discord.js with sample bot

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log('Hello, World!')
+import * as fs from "fs";
+import * as Discord from "discord.js";
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+	if (msg.content === 'ping') {
+		msg.reply('pong');
+	}
+});
+
+let config = JSON.parse(fs.readFileSync('./config/config.json', 'utf8'));
+client.login(config.token);
