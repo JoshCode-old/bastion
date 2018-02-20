3c1d383795cbe43b2b286e903623f2dac49ffd0d
Unknown <joshcodedev@gmail.com>
2018-02-20 23:48:06 +0100

    Add toggle for debug server and production server mode

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,24 @@ import * as fs from "fs";
 import * as Discord from "discord.js";
 const client = new Discord.Client();
 
+let config = JSON.parse(fs.readFileSync('./config/config.json', 'utf8'));
+
+let enabledGuilds : string[] = [];
+if(config.debug.enabled)
+	enabledGuilds.push(config.debug.debugServer);
+else
+	enabledGuilds.push(config.productionServer);
+
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-	if (msg.content === 'ping') {
-		msg.reply('pong');
+	if (enabledGuilds.includes(msg.guild.id)) {
+		if (msg.content === 'ping') {
+			msg.reply('pong');
+		}
 	}
 });
 
-let config = JSON.parse(fs.readFileSync('./config/config.json', 'utf8'));
 client.login(config.token);
