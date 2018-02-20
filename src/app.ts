ff140f4ba490a069a0140d613514a24c148f6c52
Unknown <joshcodedev@gmail.com>
2018-02-21 00:39:58 +0100

    Change debug server to debug channel

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,39 @@
 import * as fs from "fs";
 import * as Discord from "discord.js";
+import {BotCommand} from "./commands/BotCommand";
+import {AboutCommand} from "./commands/AboutCommand";
+
 const client = new Discord.Client();
 
 let config = JSON.parse(fs.readFileSync('./config/config.json', 'utf8'));
 
-let enabledGuilds : string[] = [];
-if(config.debug.enabled)
-	enabledGuilds.push(config.debug.debugServer);
-else
-	enabledGuilds.push(config.productionServer);
+let enabledChannels: string[] = [];
+let enabledCommands: BotCommand[] = [];
+
+if (config.debug.enabled)
+	enabledChannels.push(config.debug.debugChannel);
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-	if (enabledGuilds.includes(msg.guild.id)) {
+	if(config.debug.enabled) {
+		if (msg.content == '!enable') {
+			enabledChannels.push(msg.channel.id)
+		}
+		if (msg.content == '!disable') {
+			let index = enabledChannels.indexOf(msg.channel.id);
+			if(index > -1) {
+				enabledChannels.splice(index, 1);
+			}
+		}
+	}
+	if (enabledChannels.includes(msg.channel.id) || config.debug.enabled === false) {
+		for (let i = 0; i < enabledCommands.length; i++) {
+			if (msg.content === enabledCommands[i].commandName)
+				enabledCommands[i].handle(msg)
+		}
 		if (msg.content === 'ping') {
 			msg.reply('pong');
 		}
