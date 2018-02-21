b5ce368a575ffb6b1e28a71dde594e651b6e7f87
JoshCode <joshcodedev@gmail.com>
2018-02-21 18:39:53 +0100

    Restructuring of the !info command
    Now sends an 'in progress' message while it's working on the API call

--- src/commands/InfoCommand.ts
+++ src/commands/InfoCommand.ts
@@ -11,50 +11,59 @@ export class InfoCommand extends BotCommand {
 
 	handle(msg: Message) {
 		let response = new ResponseEmbed();
-		let args: string[] = msg.content.split(" ");
-		args.splice(0, 1);
+		let sentMessage = null;
+		response.setDescription("Your request is being processed");
+		response.complete(false);
+		response.setColor("#0000FF");
+		msg.channel.send(response).then((message: Message) => {
+			sentMessage = message;
 
-		const username = args[0].replace("#", "-");
-		const options = {
-			hostname: 'owapi.net',
-			path: `/api/v3/u/${username}/stats`,
-			method: 'GET',
-			headers: {
-				'User-Agent': `nl.codefox.bastion ${process.env.npm_package_version}`
-			}
-		};
+			let args: string[] = msg.content.split(" ");
+			args.splice(0, 1);
 
-		const req = https.request(options, (res: IncomingMessage) => {
-			console.log(`${res.statusCode} ${res.statusMessage}`);
-			res.setEncoding('utf8');
-			let responseString: string = "";
-			res.on('data', (chunk) => {
-				responseString += chunk;
-			});
-			res.on('end', () => {
-				let responseJson = JSON.parse(responseString);
-				if (res.statusCode === 200) {
-					response.addField("Username", `${username.substr(0, username.length - 5)}`, false);
-					response.addField("Tier", `${responseJson.eu.stats.competitive.overall_stats.tier}`, true);
-					response.addField("Rank", `${responseJson.eu.stats.competitive.overall_stats.comprank}`, true);
-					response.complete(true);
-					msg.channel.send(response);
-				} else {
-					if (responseJson.msg === "profile not found") {
-						response.setDescription("This user was not found");
-					} else {
-						response.setDescription("Unexpected error handling your request");
-					}
-					response.complete(false);
-					msg.channel.send(response);
+			const username = args[0].replace("#", "-");
+			const options = {
+				hostname: 'owapi.net',
+				path: `/api/v3/u/${username}/stats`,
+				method: 'GET',
+				headers: {
+					'User-Agent': `nl.codefox.bastion ${process.env.npm_package_version}`
 				}
-			})
-		});
+			};
 
-		req.on('error', () => {
-			console.log("Error submitting API request")
+			const req = https.request(options, (res: IncomingMessage) => {
+				console.log(`${res.statusCode} ${res.statusMessage}`);
+				res.setEncoding('utf8');
+				let responseString: string = "";
+				res.on('data', (chunk) => {
+					responseString += chunk;
+				});
+				res.on('end', () => {
+					let responseJson = JSON.parse(responseString);
+					if (res.statusCode === 200) {
+						response.setDescription("");
+						response.addField("Username", `${username.substr(0, username.length - 5)}`, false);
+						response.addField("Tier", `${responseJson.eu.stats.competitive.overall_stats.tier}`, true);
+						response.addField("Rank", `${responseJson.eu.stats.competitive.overall_stats.comprank}`, true);
+						response.complete(true);
+						sentMessage.edit(response);
+					} else {
+						if (responseJson.msg === "profile not found") {
+							response.setDescription("This user was not found");
+						} else {
+							response.setDescription("Unexpected error handling your request");
+						}
+						response.complete(false);
+						sentMessage.edit(response);
+					}
+				})
+			});
+
+			req.on('error', () => {
+				console.log("Error submitting API request")
+			});
+			req.end();
 		});
-		req.end();
 	}
 
 }
