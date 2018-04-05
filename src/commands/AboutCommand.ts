b1f6c08ff0d60f047b8922e51ca8212898d9b04a
JoshCode <joshcodedev@gmail.com>
2018-04-05 14:24:22 +0200

    Fix version in AboutCommand

--- src/commands/AboutCommand.ts
+++ src/commands/AboutCommand.ts
@@ -1,6 +1,7 @@
 import {BotCommand} from "./BotCommand";
 import {Message} from "discord.js";
 import {ResponseEmbed} from "../util/ResponseEmbed";
+import {version} from "../app";
 
 export class AboutCommand extends BotCommand {
 	commandName: string = "!about";
@@ -10,7 +11,7 @@ export class AboutCommand extends BotCommand {
 	public handle(msg: Message) {
 		let response = new ResponseEmbed();
 
-		response.addField("Version", process.env.npm_package_version, false);
+		response.addField("Version", version, false);
 		response.addField("Repository", "https://www.github.com/JoshCode/B4ST10N", false);
 
 		response.complete(true);
